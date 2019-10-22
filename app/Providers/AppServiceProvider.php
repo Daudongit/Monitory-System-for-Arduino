dc05e8959c03ea637f8afb0c7b011e58f980b8ba
Daud <daudonmail@gmail.com>
2019-10-22 01:10:38 +0100

    force https for heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
